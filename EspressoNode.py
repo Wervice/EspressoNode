6dee254e4c41f3c7d274f3823d222b895607a54e
wervice <wervice@proton.me>
2023-05-31 14:25:20 +0200

    Small Design Update
    I moved the buttons a bit and I renamed the help button.

--- EspressoNode.py
+++ EspressoNode.py
@@ -39,13 +39,12 @@ def run():
 def help():
     filename = 'file:///'+os.getcwd()+'/' + 'list.html'
     webbrowser.open_new_tab(filename)
-
 Label(window, text="EspressoNode", height=1, width=100, font=("", 15), background="#9141ac", foreground="white", anchor="nw").place(x=0, y=0)
 combo = ttk.Combobox(values = ['Disable', 'AD', 'AN', 'AE','AF','AG','AI','AL','AM','AO','AQ','AR','AS','AT','AU','AW','AX','AZ','BA','BB','BD','BE','BF','BG','BH','BI','BJ','BL','BM','BN','BO','BQ','BR','BS','BT','BV','BW','BY','BZ','CA','CC','CD','CF','CG','CH','CI','CK','CL','CM','CN','CO','CR','CU','CV','CW','CX','CY','CZ','DE','DJ','DK','DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FM','FO','FR','GA','GB','GD','GE','GF','GG','GH','GI','GL','GM','GN','GP','GQ','GR','GS','GT','GU','GW','GY','HK','HM','HN','HR','HT','HU','ID','IE','IL','IM','IN','IO','IQ','IR','IS','IT','JE','JM','JO','JP','KE','KG','KH','KI','KM','KN','KP','KR','KW','KY','KZ','LA','LB','LC','LI','LK','LR','LS','LT','LU','LV','LY','MA','MC','MD','ME','MF','MG','MH','MK','ML','MM','MN','MO','MP','MQ','MR','MS','MT','MU','MV','MW','MX','MY','MZ','NA','NC','NE','NF','NG','NI','NL','NO','NP','NR','NU','NZ','OM','PA','PE','PF','PG','PH','PK','PL','PM','PN','PR','PS','PT','PW','PY','QA','RE','RO','RS','RU','RW','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SK','SL','SM','SN','SO','SR','SS','ST','SV','SX','SY','SZ','TC','TD','TF','TG','TH','TJ','TK','TL','TM','TN','TO','TR','TT','TV','TW','TZ','UA','UG','UM','US','UY','UZ','VA','VC','VE','VG','VI','VN','VU','WF','WS','YE','YT','ZA','ZM','ZW'])
 combo.place(x=10, y=40)
-button_go = ttk.Button(master=window, text="Go", command=run)
+button_go = ttk.Button(master=window, text="Run", command=run)
 button_go.place(x=10, y=80)
-button_help = ttk.Button(master=window, text="Country codes list", command=help)
-button_help.place(x=10, y=120)
+button_help = ttk.Button(master=window, text="Help", command=help)
+button_help.place(x=110, y=80)
 
 mainloop()
